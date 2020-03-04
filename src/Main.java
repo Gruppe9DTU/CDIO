e603638e283f57ab4f0d65afbaffcd057514371a
niko5297 <s185082@student.dtu.dk>
2020-03-04 11:31:36 +0100

    Added GrabMechanism class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.Motor;
+import lejos.utility.Delay;
+
 public class Main {
 
+
+
     public static void main(String[] args) {
+
+        GrabMechanism grabMechanism = new GrabMechanism();
+
+        grabMechanism.grabBall();
+
 
     }
 }
