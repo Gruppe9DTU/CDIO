a3b02e3b854f8b49d2086b09b51c396b8a589ac3
kna <s185125@student.dtu.dk>
2020-03-11 11:33:40 +0100

    Attempt to use LocalEV3 instead of RemoteEV3

--- src/controller/MotorController.java
+++ src/controller/MotorController.java
@@ -1,52 +1,61 @@
 package controller;
 
 import lejos.hardware.Button;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
 import lejos.remote.ev3.RMIRegulatedMotor;
 import lejos.remote.ev3.RemoteEV3;
 
 import java.rmi.RemoteException;
 
+/**
+ * Inspiration: http://www.robogalaxy.com/post/remote-control-of-ev3-robot-using-java
+ */
 public class MotorController {
-    RemoteEV3 robot;
+//    RemoteEV3 robot;
+    LocalEV3 robot;
     boolean driving, direction;
-    RMIRegulatedMotor rightwheel, leftwheel;
+//    RMIRegulatedMotor rightwheel, leftwheel;
+    EV3LargeRegulatedMotor rightwheel, leftwheel;
 
-    public MotorController(RemoteEV3 robot) {
-        this.robot = robot;
+    public MotorController() {
+//        this.robot = robot;
         this.driving = false;
         this.direction = false; //true = forwards, false = backwards
-//        Port portA = robot.getPort("A");
-//        Port portB = robot.getPort("D");
 
-        this.rightwheel = robot.createRegulatedMotor("A", 'L');
-        this.leftwheel = robot.createRegulatedMotor("D", 'L');
+//        this.rightwheel = robot.createRegulatedMotor("A", 'L');
+//        this.leftwheel = robot.createRegulatedMotor("D", 'L');
+
+        this.rightwheel = new EV3LargeRegulatedMotor(MotorPort.A);
+        this.leftwheel = new EV3LargeRegulatedMotor(MotorPort.D);
 
     }
 
     public void wiggleTest(int wiggleness) {
         while(Button.ESCAPE.isUp() && driving) {
             wiggleness = directionHandling(wiggleness);
-            try {
+//            try {
                 rightwheel.rotate(wiggleness);
                 leftwheel.rotate(wiggleness);
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            }
+//            } catch (RemoteException e) {
+//                e.printStackTrace();
+//            }
         }
     }
 
     public void driveTest(int fastness) {
         while(Button.ESCAPE.isUp() && driving) {
             fastness = directionHandling(fastness);
-            try {
+//            try {
                 rightwheel.setSpeed(fastness);
                 leftwheel.setSpeed(fastness);
 
                 rightwheel.forward();
                 leftwheel.forward();
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            }
+//            } catch (RemoteException e) {
+//                e.printStackTrace();
+//            }
         }
     }
 
