83d688e110b06a832bf4d437374c96100e4af2ac
kna <s185125@student.dtu.dk>
2020-03-11 11:00:42 +0100

    Two tests for running the motors - rotate - forward (with speed)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import controller.MotorController;
+import lejos.hardware.BrickFinder;
+import lejos.hardware.Button;
+import lejos.remote.ev3.RemoteEV3;
+
 public class Main {
 
     public static void main(String[] args) {
 
+
+        //TODO All below is for testing purpose, delete when ready
+        MotorController motor = new MotorController((RemoteEV3) BrickFinder.getDefault());
+        motor.setDriving(true);
+//        motor.wiggleTest(30);
+        motor.driveTest(1000);
+
+        Button.waitForAnyPress();
+        motor.setDriving(false);
     }
 }
--- src/controller/MotorController.java
+++ src/controller/MotorController.java
@@ -1,4 +1,68 @@
 package controller;
 
+import lejos.hardware.Button;
+import lejos.remote.ev3.RMIRegulatedMotor;
+import lejos.remote.ev3.RemoteEV3;
+
+import java.rmi.RemoteException;
+
 public class MotorController {
+    RemoteEV3 robot;
+    boolean driving, direction;
+    RMIRegulatedMotor rightwheel, leftwheel;
+
+    public MotorController(RemoteEV3 robot) {
+        this.robot = robot;
+        this.driving = false;
+        this.direction = false; //true = forwards, false = backwards
+//        Port portA = robot.getPort("A");
+//        Port portB = robot.getPort("D");
+
+        this.rightwheel = robot.createRegulatedMotor("A", 'L');
+        this.leftwheel = robot.createRegulatedMotor("D", 'L');
+
+    }
+
+    public void wiggleTest(int wiggleness) {
+        while(Button.ESCAPE.isUp() && driving) {
+            wiggleness = directionHandling(wiggleness);
+            try {
+                rightwheel.rotate(wiggleness);
+                leftwheel.rotate(wiggleness);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void driveTest(int fastness) {
+        while(Button.ESCAPE.isUp() && driving) {
+            fastness = directionHandling(fastness);
+            try {
+                rightwheel.setSpeed(fastness);
+                leftwheel.setSpeed(fastness);
+
+                rightwheel.forward();
+                leftwheel.forward();
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Handles the motor going the right direction, giving the coder an advantage of writing positive numbers for forward and negative for backwards
+     * @param speed speed of wheels
+     */
+    private int directionHandling(int speed) {
+        return direction ? speed : -speed;
+    }
+
+    /**
+     * Toggle driving on/off
+     * @param isDriving if robot is driving
+     */
+    public void setDriving(boolean isDriving) {
+        driving = isDriving;
+    }
 }
